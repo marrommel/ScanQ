8353481f03505340d6977ef5f0dbae61054d6736
marro <marrommel@outlook.de>
2025-05-29 20:49:58 +0200

    added warning if survey is attempted on unsupported OS

--- lib/home/activity_home.dart
+++ lib/home/activity_home.dart
@@ -78,7 +78,7 @@ System: ${iosInfo.systemName}
       await launchUrl(surveyUri);
     } else {
       ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(AppLocalizations.of(context)!.savedVocabsSuccessfully)));
+          .showSnackBar(SnackBar(content: Text(AppLocalizations.of(context)!.surveyUnavailable)));
     }
   }
 
